61ea1d62315c548a4a19ca4c1a5f63efa11c25f0
ayushch0001 <ayushch0001@gmail.com>
2026-05-31 10:47:20 +0530

    form bhiya side also updated

--- autoaudit/settings.py
+++ autoaudit/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = 'django-insecure-(d5g(r!)%umck6flvg4pf0+bxj1a13w=x&ho0vptb7a@#40_bh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cavijay.attendoo.online','185.170.196.189','127.0.0.1',''192.168.10.100'']
+ALLOWED_HOSTS = ['cavijay.attendoo.online','185.170.196.189','127.0.0.1','192.168.10.100','divisive-ream-doormat.ngrok-free.dev']
 CSRF_TRUSTED_ORIGINS = [
-    'http://185.170.196.189:8000','https://cavijay.attendoo.online',''http://192.168.10.100'','http://192.168.10.100/']
+    'http://185.170.196.189:8000','https://cavijay.attendoo.online','http://192.168.10.100','http://192.168.10.100/','https://divisive-ream-doormat.ngrok-free.dev']
 CSRF_COOKIE_SECURE = False
 
 
